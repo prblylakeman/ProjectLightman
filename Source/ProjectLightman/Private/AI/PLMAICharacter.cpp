6083321d9bd1dbdb97d3b0b7d770c1ee802d75ee
kevin_neilson <neilsonsound@gmail.com>
2023-07-02 16:00:11 -0700

    new ai classes, changing idle mesh in main menu

---
+++ Source/ProjectLightman/Private/AI/PLMAICharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/PLMAICharacter.h"
+
+// Sets default values
+APLMAICharacter::APLMAICharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APLMAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APLMAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APLMAICharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/ProjectLightman/Public/AI/PLMAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "PLMAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLIGHTMAN_API APLMAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
